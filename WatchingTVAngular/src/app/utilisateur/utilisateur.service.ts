a13a63c8aacaea9e309eb020780883d65c584314
Ludovic <ludoverfaille@outlook.com>
2018-11-21 09:34:21 +0100

    test creation Utilisateur

---
+++ WatchingTVAngular/src/app/utilisateur/utilisateur.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'xrjs';
+import {Utilisateur} "./utilisateur";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilisateurService {
+
+  constructor() { }
+}
---
+++ WatchingTVAngular/src/app/utilisateur/utilisateur.ts
@@ -0,0 +1,4 @@
+export class Utilisateur {
+  private _id:number;
+  private _
+}
