981ade2783b91258f1586a45f75fbde3f796990b
Ludovic <ludoverfaille@outlook.com>
2018-11-21 09:58:20 +0100

    Utilisateur et utilisateur.service complet

--- WatchingTVAngular/src/app/utilisateur/utilisateur.service.ts
+++ WatchingTVAngular/src/app/utilisateur/utilisateur.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Observable} from 'xrjs';
-import {Utilisateur} "./utilisateur";
+import {Observable} from 'rxjs';
+import {Utilisateur} from "./utilisateur";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
@@ -8,5 +8,23 @@ import {HttpClient} from "@angular/common/http";
 })
 export class UtilisateurService {
 
-  constructor() { }
+  public static readonly URL_API_UTILISATEUR:string="/api/utilisateur";
+
+  constructor(public http:HttpClient) { }
+
+  public query():Observable<Utilisateur[]>{
+    return this.http.get<Utilisateur[]>(UtilisateurService.URL_API_UTILISATEUR);
+  }
+
+  public post(utilisateur:Utilisateur): Observable<Utilisateur>{
+    return this.http.post<Utilisateur>(UtilisateurService.URL_API_UTILISATEUR, utilisateur.toJson());
+  }
+
+  public update(utilisateur:Utilisateur): Observable<any>{
+    return this.http.put(UtilisateurService.URL_API_UTILISATEUR, utilisateur.toJson());
+  }
+
+  public delete(utilisateur:Utilisateur): Observable<any>{
+    return this.http.delete<any>(UtilisateurService.URL_API_UTILISATEUR + "/" + utilisateur.id);
+  }
 }
--- WatchingTVAngular/src/app/utilisateur/utilisateur.ts
+++ WatchingTVAngular/src/app/utilisateur/utilisateur.ts
@@ -1,4 +1,59 @@
 export class Utilisateur {
   private _id:number;
-  private _
+  private _username:string;
+  private _isAdmin:boolean;
+  private _password:string;
+
+  constructor(username: string ="", isAdmin: boolean = false, password: string){
+    this._username = username;
+    this._isAdmin = isAdmin;
+    this._password = password;
+  }
+
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get username(): string {
+    return this._username;
+  }
+
+  set username(value: string) {
+    this._username = value;
+  }
+
+  get isAdmin(): boolean {
+    return this._isAdmin;
+  }
+
+  set isAdmin(value: boolean) {
+    this._isAdmin = value;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  set password(value: string) {
+    this._password = value;
+  }
+
+  public fromJson(Json:any):Utilisateur{
+    Object.assign(this,Json);
+    return this;
+  }
+
+  public toJson():any{
+    return{
+      "id": this._id,
+      "username": this._username,
+      "isAdmin": this._isAdmin,
+      "password": this._password
+    }
+  }
 }
